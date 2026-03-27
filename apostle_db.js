e16597d4e082ba7f39e5c2d66ba5b41588c52811
hn01182-hue <hn01182@gmail.com>
2026-03-27 12:07:54 +0900

    네르(빡침) 추가

--- apostle_db.js
+++ apostle_db.js
@@ -3,6 +3,7 @@ const apostleDB = [
     { id: "그윈", name: "그윈", role: "서포터", type: "냉정", isEldain: false },
     { id: "나이아", name: "나이아", role: "서포터", type: "순수", isEldain: false },
     { id: "네르", name: "네르", role: "서포터", type: "광기", isEldain: false },
+    { id: "네르(빡침)", name: "네르(빡침)", role: "딜러", type: "활발", isEldain: true },
     { id: "네티", name: "네티", role: "탱커", type: "광기", isEldain: false },
     { id: "다야", name: "다야", role: "딜러", type: "순수", isEldain: false },
     { id: "다야(퓨어샤인)", name: "다야(퓨어샤인)", role: "서포터", type: "광기", isEldain: false },
