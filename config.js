650cf0990308fdd2989c8e0eba86a9e8d0f4eefc
hn01182-hue <hn01182@gmail.com>
2026-04-10 09:07:49 +0900

    Update config.js

--- config.js
+++ config.js
@@ -82,7 +82,8 @@ const defaultConfig = {
 			{ id: 'seven_select', name: '영원살이! 일곱자매 선택권', val: 10700, icon: 'images/영원살이! 일곱자매 선택권.png', count: "" },
 			{ id: 'mileage', name: '마일리지', val: 10, icon: 'images/마일리지.png', count: "" },
 			{ id: 'wildcard', name: '전설 와일드 카드', val: 0, icon : 'images/전설 와일드 카드.webp', count:""},
-			{ id: 'l_card_select', name: '보유 전설 카드 선택 상자', val: 0, icon : 'images/보유 전설 카드 선택 상자.webp', count:""}
+			{ id: 'l_card_select', name: '보유 전설 카드 선택 상자', val: 0, icon : 'images/보유 전설 카드 선택 상자.webp', count:""},
+			{ id: 'excellent_token', name: '우수 증표', val: 300, icon: 'images/우수 증표.png', count: "" }
         ]    
     };
 let config = JSON.parse(JSON.stringify(defaultConfig));
